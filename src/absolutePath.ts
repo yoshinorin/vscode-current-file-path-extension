30201fc4c3a2c45772409fa407ef93cccd586b10
YoshinoriN <nayo-msoft@outlook.jp>
2018-05-02 10:03:03 +0900

    Add PathType setting

--- src/absolutePath.ts
+++ src/absolutePath.ts
@@ -1,15 +1,17 @@
 'use strict';
 
-import {StatusBarAlignment, StatusBarItem, window } from 'vscode';
+import {StatusBarAlignment, StatusBarItem, window, workspace } from 'vscode';
 
 export class AbsolutePath {
 
-    private _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+    private _config: any;
     private _unixLikePath: string = "";
     private _windowsLikePath: string = "";
+    private _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
 
     constructor() {
         let editor = window.activeTextEditor;
+        this._config = workspace.getConfiguration("absolutepath");
 
         if (!editor) {
             this._statusBarItem.hide();
@@ -18,8 +20,20 @@ export class AbsolutePath {
 
         this._unixLikePath = editor.document.uri.fsPath.replace(/\\/g, "/");
         this._windowsLikePath = editor.document.uri.fsPath.replace(/\//g, "\\");
-        this._statusBarItem.text = this._unixLikePath;
+        if (this.isUnixLike) {
+            this._statusBarItem.text = this._unixLikePath;
+        } else {
+            this._statusBarItem.text = this._windowsLikePath;
+        }
         this._statusBarItem.show();
+    }
+
+    private isUnixLike(): boolean {
+        //TODO: Use enum
+        if (this._config.pathType === "unix") {
+            return true;
+        }
+        return false;
     }
 
     dispose() {
