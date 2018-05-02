745d9c1ebcd0da89f98f9c7c075241721c70eb6a
YoshinoriN <nayo-msoft@outlook.jp>
2018-05-02 22:01:22 +0900

    Delete console.log

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import { ExtensionContext } from 'vscode';
 import { AbsolutePath } from './absolutePath';
 
 export function activate(context: ExtensionContext) {
-    console.log('Congratulations, your extension "display-absolute-path" is now active!');
 
     let absolutePath = new AbsolutePath();
     context.subscriptions.push(absolutePath);
