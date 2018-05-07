e4cc862e5f34a6b7242ff75dbacd5d282dbec299
YoshinoriN <nayo-msoft@outlook.jp>
2018-05-07 22:21:18 +0900

    Implement interface for QuickPickItem

--- src/quickPicker.ts
+++ src/quickPicker.ts
@@ -2,24 +2,39 @@
 
 import { QuickPickItem, window } from 'vscode';
 
+interface MenuQuickPickItem extends QuickPickItem {
+    id: QuickPickerAction;
+}
+
+enum QuickPickerAction {
+    viewUnixStyle,
+    viewWindowsStyle,
+    copy,
+    openWithExplorer
+}
+
 export class QuickPicker {
 
-    private _pickItems: QuickPickItem[] = [];
+    private _pickItems: MenuQuickPickItem[] = [];
 
     constructor() {
         this._pickItems.push({
+            id: QuickPickerAction.viewUnixStyle,
             label: "UNIX style",
             detail: "View on UNIX style path.",
         });
         this._pickItems.push({
+            id: QuickPickerAction.viewWindowsStyle,
             label: "Windows style",
             detail: "View on Windows style path.",
         });
         this._pickItems.push({
+            id: QuickPickerAction.copy,
             label: "COPY",
             detail: "Copy a current file full path.",
         });
         this._pickItems.push({
+            id: QuickPickerAction.openWithExplorer,
             label: "Open with Explorer",
             detail: "Open a current file with Explorer.",
         });
