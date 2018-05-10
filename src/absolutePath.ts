06bb10c88c1c382715676a20d01795afb4f9bd29
YoshinoriN <nayo-msoft@outlook.jp>
2018-05-11 00:10:40 +0900

    Refactoring some instance member of AbsolutePath class

--- src/absolutePath.ts
+++ src/absolutePath.ts
@@ -9,10 +9,24 @@ export class AbsolutePath {
 
     private _config: Config;
     private _quickPicker: QuickPicker;
-    private _currentStyle: string = "";
     private _unixLikePath: string = "";
     private _windowsLikePath: string = "";
     private _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+
+    private _currentStyle: string = "";
+    public get currentStyle() : string {
+        if (this._currentStyle === PathStyles.UNIX) {
+            return PathStyles.UNIX;
+        }
+        return PathStyles.WINDOWS;
+    }
+
+    public get currentPath() : string {
+        if (this.currentStyle === PathStyles.UNIX) {
+            return this._unixLikePath;
+        }
+        return this._windowsLikePath;
+    }
 
     constructor() {
         this._config = new Config();
@@ -33,11 +47,7 @@ export class AbsolutePath {
         this._unixLikePath = editor.document.uri.fsPath.replace(/\\/g, "/");
         this._windowsLikePath = editor.document.uri.fsPath.replace(/\//g, "\\");
 
-        if (this.isUnixLike()) {
-            this._statusBarItem.text = this._unixLikePath;
-        } else {
-            this._statusBarItem.text = this._windowsLikePath;
-        }
+        this._statusBarItem.text = this.currentPath;
         this._statusBarItem.show();
     }
 
@@ -47,11 +57,10 @@ export class AbsolutePath {
         });
     }
 
-    private isUnixLike(): boolean {
-        if (this._currentStyle === PathStyles.UNIX) {
-            return true;
-        }
-        return false;
+                default:
+                    return;
+            }
+        });
     }
 
     dispose() {
