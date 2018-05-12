6a10748d5037ebe3e6f6de23fbd06efdee233aa6
YoshinoriN <nayo-msoft@outlook.jp>
2018-05-12 21:39:56 +0900

    Change tooltip content

--- src/absolutePath.ts
+++ src/absolutePath.ts
@@ -34,7 +34,7 @@ export class AbsolutePath {
         this._quickPicker = new QuickPicker();
         this._currentStyle = this._config.defaultPathStyle;
         this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, this._config.priorityInStatusBar);
-        this._statusBarItem.tooltip = "Show Menus";
+        this._statusBarItem.tooltip = "Open Menus";
         this._statusBarItem.command = 'absolutePath.executeQuickPickerAction';
         this.display();
     }
